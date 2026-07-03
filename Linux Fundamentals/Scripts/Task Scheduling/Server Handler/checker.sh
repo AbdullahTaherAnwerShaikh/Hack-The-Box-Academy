e92ec562ea72505ef1c232b8de49d362c3e9058d
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-07-03 19:05:26 +0300

    Correct if statement syntax in checker.sh
    Fix syntax error in if statement for server check.

--- Linux Fundamentals/Scripts/Task Scheduling/Server Handler/checker.sh
+++ Linux Fundamentals/Scripts/Task Scheduling/Server Handler/checker.sh
@@ -4,7 +4,7 @@
 pid=$(cat /tmp/currentServer.pid)
 
 #checks whether server is running or not, if running echos message and if not it will restart
-if kill -0 $pid then;
+if kill -0 $pid 2>/dev/null; then
     echo "Server is running as intended with PID: $pid"
 else
     ./starter.sh --restart&
