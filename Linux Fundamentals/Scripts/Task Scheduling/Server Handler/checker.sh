9814414b45ee354509723dc47d08d23a00dc1966
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2026-07-03 18:37:41 +0300

    Add checker.sh to monitor server status
    This script checks if the server is running and restarts it if not.

---
+++ Linux Fundamentals/Scripts/Task Scheduling/Server Handler/checker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#retrieves pid of the latest server
+pid=$(cat /tmp/currentServer.pid)
+
+#checks whether server is running or not, if running echos message and if not it will restart
+if kill -0 $pid then;
+    echo "Server is running as intended with PID: $pid"
+else
+    ./starter.sh --restart&
+fi
